368464e96691b4b40d1d58fa8bd3b8192d89f8d1
johnny630 <bakolama@gmail.com>
2021-12-14 18:00:52 +0800

    Submitting data

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -49,6 +49,12 @@ const Checkout = (props) => {
     }
 
     // submit cart data
+    props.onConfirm({
+      name: enteredName,
+      street: enteredStreet,
+      city: enteredCity,
+      postal: enteredPostal,
+    });
   };
 
   const nameClasses = `${classes.control} ${
